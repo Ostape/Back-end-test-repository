b8784ad0719f45970347a44f025372273587d117
Orest Shemeliuk <orestshemeliuk@gmail.com>
2021-03-13 16:03:22 +0200

    initial commit

---
+++ src/main/kotlin/com/robosh/calculator/Calculator.kt
@@ -0,0 +1,21 @@
+package com.robosh.calculator
+
+class Calculator {
+
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun minus(a: Int, b: Int): Int {
+        return a - b
+    }
+
+    fun get1(): List<Calculator> {
+        TODO()
+    }
+
+    fun get2(): MutableList<Calculator> {
+        TODO()
+    }
+
+}
---
+++ src/test/kotlin/com/robosh/calculator/CalculatorTest.kt
@@ -0,0 +1,33 @@
+package com.robosh.calculator
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class CalculatorTest {
+
+    private val subject = Calculator()
+
+    @Test
+    fun `sum should return 20`() {
+        val result = subject.sum(10, 10)
+        assertEquals(result, 20)
+    }
+
+    @Test
+    fun `sum should return 10`() {
+        val result = subject.sum(5, 5)
+        assertEquals(result, 10)
+    }
+
+    @Test
+    fun `sum should return 0`() {
+        val result = subject.sum(1, -1)
+        assertEquals(result, 0)
+    }
+
+    @Test
+    fun `sum should return -20`() {
+        val result = subject.sum(-10, -10)
+        assertEquals(result, -20)
+    }
+}
