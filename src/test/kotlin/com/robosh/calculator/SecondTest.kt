a89d9af5bc9234d0b16269a000f958b7223849b0
Orest Shemeliuk <orestshemeliuk@gmail.com>
2021-05-03 09:07:17 +0300

    add second test

---
+++ src/test/kotlin/com/robosh/calculator/SecondTest.kt
@@ -0,0 +1,12 @@
+package com.robosh.calculator
+
+import junit.framework.Assert.assertTrue
+import org.junit.Test
+
+class SecondTest {
+
+    @Test
+    fun someTest() {
+        assertTrue(true)
+    }
+}
